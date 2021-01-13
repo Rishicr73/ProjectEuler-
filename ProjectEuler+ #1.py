22b4879660965c1a5fa1013f9ef976af1c5e635d
Rishicr73 <72937372+Rishicr73@users.noreply.github.com>
2021-01-13 21:55:23 +0530

    Update ProjectEuler+ #1.py

--- ProjectEuler+ #1.py
+++ ProjectEuler+ #1.py
@@ -12,7 +12,7 @@ for a0 in range(t):
     
     #sum of arithmetic equation formula = n/2(2a + (n - 1)d)
     #here a == d
-    #so we  can write this formula as = (a * n * (n-1))/2
+    #so we  can write this formula as = (a * n * (n+1))/2
     
     sum_ = (3 * three * (three + 1)) // 2 + (5 * five * (five + 1)) // 2 - (15 * fifteen * (fifteen + 1)) // 2
     
