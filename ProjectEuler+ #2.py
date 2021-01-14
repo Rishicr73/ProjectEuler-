bdd037958baadc1b78b9b1bc7e58d56d310e9f61
Rishicr73 <72937372+Rishicr73@users.noreply.github.com>
2021-01-14 23:10:31 +0530

    Add files via upload

---
+++ ProjectEuler+ #2.py
@@ -0,0 +1,58 @@
+ 
+#!/bin/python3
+
+import sys
+
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+    a = 1
+    b = 2
+    sum = 0
+    while (a <= n):
+        if a % 2 == 0:
+            sum += a
+            
+        
+        c = a
+        a = b
+        b = c + b
+        
+        
+    print(sum)         
+
+
+        
+          
+        
+        
+
+    
+
+
+
+
+        
+            
+
+  
+
+
+    
+
+
+    
+        
+
+
+    
+
+        
+    
+    
+   
+   
+
+
+
