deba355450b8295ac95d8f6a199a1822e767ea74
Rishicr73 <72937372+Rishicr73@users.noreply.github.com>
2021-01-16 13:20:09 +0530

    Update ProjectEuler+ #3.py

--- ProjectEuler+ #3.py
+++ ProjectEuler+ #3.py
@@ -1,26 +1,23 @@
  
+ 
 #!/bin/python3
 
 import sys
 
 
-t = int(input().strip())
-for a0 in range(t):
-    n = int(input().strip())
-    a = 1
-    b = 2
-    sum = 0
-    while (a <= n):
-        if a % 2 == 0:
-            sum += a
-            
-        
-        c = a
-        a = b
-        b = c + b
-        
-        
-    print(sum)         
+T = int(input())
+for z in range(T):
+    N = int(input())
+    i = 2
+    largest_prime = 2
+    while i * i <= N:   #here we have taken i*i to reduce time complexity
+        while N % i == 0:
+            largest_prime = i
+            N //= i    
+        i += 1
+    if N>largest_prime:
+        largest_prime = N
+    print(largest_prime)         
 
 
         
